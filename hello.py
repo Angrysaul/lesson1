edf139d6841163b701b0f017024d9419a0234ac5
Angrysaul <4256222@gmail.com>
2020-09-09 21:28:06 +0300

    Паервая версия

---
+++ hello.py
@@ -0,0 +1,9 @@
+def get_sum(one, two, delimiter='&'):
+    one_two = one + delimiter + two
+    print(one_two.title()) 
+get_sum("Learn", "python")
+#print(get_sum)
+#one = "Learn"
+#two = "Python"
+#print(one + "&" + two)
+#get_summ(one, two, delimiter='&')
